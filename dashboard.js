5896893eeac8615cecef6c9ec21f2415eea4637b
MD RAKIB HOSSEN <mrrakib5007@gmail.com>
2023-08-16 01:49:54 +0600

    JS added

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,27 @@
+
 // deposit button Event Listener 
 document.getElementById('deposit-btn').addEventListener('click', function(){
+    
+    // Deposit Input Amount Getting 
     const depositInputBox = document.getElementById('depositInputBox');
     const depositAmount   = depositInputBox.value;
-    // alert(depositAmount);
+    
+    // Deposit Total Box
+    const depositBox    = document.getElementById('depositBox');
+    const depositTotal  = depositBox.innerText;
+
+
+    // Total Balance Box
+    const balanceBox    = document.getElementById('balanceBox');
+    const balanceTotal  = balanceBox.innerText;
+
+    depositBox.innerText    = parseFloat(depositTotal) + parseFloat(depositAmount);
+    balanceBox.innerText    = parseFloat(balanceTotal) + parseFloat(depositAmount);
+
+    // Clear deposit field
+    depositInputBox.value     = '';
+
+    
 });
+
+
